6bf90d77db4815c59f9eb587d6c38e5f213778e9
Emily Lillie <emilylillie3@gmail.com>
2019-07-18 17:04:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,14 @@
 
 def roll_call_dwarves(dwarves)
-  dwarves = []
+  dwarves = %w[
+  Doc
+  Dopey
+  Bashful
+  Grumpy
+  Sneezy
+  Sleepy
+  Happy
+]
   dwarves.each_with_index do |dwarf, index|
   puts "#{index + 1}. #{dwarf}"
 end
