94dca6e4ddcef576c78fca88ff94b3229cbcd75d
Emily Lillie <emilylillie3@gmail.com>
2019-07-18 17:19:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,11 @@ def long_planeteer_calls(planeteer_calls)
    planeteer_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese(ingredients)
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  ingredients.find {|
+
+
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w[gouda cheddar camembert]
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
 end
